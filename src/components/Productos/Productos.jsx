94f689b7e74863d912b1644efba486aac6a48db8
dfranco1972 <daniel.franco@fondocnog.com.mx>
2023-10-24 13:15:05 -0600

    correccion

--- src/components/Productos/Productos.jsx
+++ src/components/Productos/Productos.jsx
@@ -1,8 +1,9 @@
 import { useProdContext } from '@/hooks/useProdContext'
+import DetallesProductos from '@/components/DetallesProductos/DetallesProductos.jsx'
 import './productos.css'
 
 const ProdList = () => {
-  const { list, loading, setSelectedProd, search } = useProdContext()
+  const { list, loading, setSelectedProd, selectedProd, search } = useProdContext()
 
   // Lista de canciones filtradas por la palabra buscada
   const filteredProdList = list.filter((Prod) => {
@@ -13,7 +14,7 @@ const ProdList = () => {
     <section className='row-container'>
       {loading
         ? <h1>Cargando...</h1>
-        : filteredProdList.map((Prod) => (
+        : Object.entries(selectedProd).length !== 0 ? <DetallesProductos/> : filteredProdList.map((Prod) => (
           <div
             className='row-Prod'
             key={Prod.id}
